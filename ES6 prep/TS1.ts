9957c4fb0834ef606c1c947a8d5515747e2218bf
Visweshvar-UST <287618@ust.com>
2024-11-09 16:22:53 +0530

    for each

---
+++ ES6 prep/TS1.ts
@@ -0,0 +1,55 @@
+/**************************
+ * foreach method
+ ***************************/
+const color = ['red','blue','green']
+
+for(let i = 0; i < color.length; i++){
+    console.log(color[i]);
+}
+// foreach
+const numbers = [1,2,3,4,5]
+let sum = 0;
+numbers.forEach((num)=>{
+    sum+=num;
+})
+
+console.log(sum);
+
+function findSum(num: number): void {
+    sum += num;
+}
+
+sum = 0
+numbers.forEach(findSum);
+console.log(sum);
+
+
+/**************************
+ * map method
+ ***************************/
+const digits = [1,2,3,4,5]
+//map 
+/**************************
+ * foreach method
+ ***************************/
+
+/**************************
+ * foreach method
+ ***************************/
+
+/**************************
+ * foreach method
+ ***************************/
+
+/**************************
+ * foreach method
+ ***************************/
+
+/**************************
+ * foreach method
+ ***************************/
+
+/**************************
+ * foreach method
+ ***************************/
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Person {
+  name: string;
+}
 
+interface Employee extends Person {
+  id: number;
 
+}
 
+const emp:Employee = {name:'viswa',id:12};
+
+console.log(emp)
 
 
 
 function App() {
   return (
     <div className="App">
+      <h1>{JSON.stringify(emp)}</h1>
     </div>
   );
 }
