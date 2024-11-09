eeefc2a2b807adf11483f971369260e1b6864dfe
Visweshvar-UST <287618@ust.com>
2024-11-09 16:35:31 +0530

    map

--- ES6 prep/TS1.ts
+++ ES6 prep/TS1.ts
@@ -6,7 +6,9 @@ const color = ['red','blue','green']
 for(let i = 0; i < color.length; i++){
     console.log(color[i]);
 }
-// foreach
+
+// foreach return void
+
 const numbers = [1,2,3,4,5]
 let sum = 0;
 numbers.forEach((num)=>{
@@ -28,10 +30,36 @@ console.log(sum);
  * map method
  ***************************/
 const digits = [1,2,3,4,5]
-//map 
+const doubleDigits = []
+for(let i = 0; i < digits.length; i++){
+    doubleDigits.push(digits[i]*2);
+}
+console.log(doubleDigits);
+
+//map  return new type val
+
+let doubled = digits.map((num)=>{return num*2});
+console.log(doubled);
+
+const cars = [
+    { mpdel: 'asd', price: 'asdaf'},
+    { mpdel: 'fdsg', price: '12ads'},
+];
+
+const price = cars.map((ele)=>{return ele.price})
+console.log(price);
+
+
 /**************************
- * foreach method
+ * filter method
  ***************************/
+const students = [
+    {name: 'viswa', major: 'computer science'},
+    {name: 'axel', major: 'computer science'},
+    {name: 'anu', major: 'computer science'},
+    {name: 'PJ', major: 'computer science'},
+]
+// filter
 
 /**************************
  * foreach method
